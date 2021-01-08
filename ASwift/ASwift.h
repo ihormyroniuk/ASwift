b528cf5f1b9f00792f681a7f7135da91479466e6
Ihor Myroniuk <ihormyroniuk@gmail.com>
2021-01-09 00:33:18 +0200

    no message

---
+++ ASwift/ASwift.h
@@ -0,0 +1,18 @@
+//
+//  ASwift.h
+//  ASwift
+//
+//  Created by Ihor Myroniuk on 09.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ASwift.
+FOUNDATION_EXPORT double ASwiftVersionNumber;
+
+//! Project version string for ASwift.
+FOUNDATION_EXPORT const unsigned char ASwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASwift/PublicHeader.h>
+
+
